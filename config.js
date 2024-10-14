e98a512187debdb64bfe9b64fad7e93ddc578cdc
Slimz08 <miloblaq08@gmail.com>
2024-10-14 18:07:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=iuoVjawJ#k8cJeXFXqyHXTApaSYsq_sz-4MuRDcjQjbNqpbleJSw",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=rmw0FKhR#88Qpj9aEdI_uzZ-nEwFJWQajBcMpDI-0D22ZciKSeAg",
 MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",//enter mongo db url
 };
